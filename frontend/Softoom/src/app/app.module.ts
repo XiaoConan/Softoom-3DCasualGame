fadb014f41cc66d2d332b5109731432a3f851d37
Zexi Zhang <47746248+Joseph1025@users.noreply.github.com>
2023-03-09 00:16:34 -0500

    Revert "Jerry: Created the 3D Male Character Model"

--- frontend/Softoom/src/app/app.module.ts
+++ frontend/Softoom/src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaleCharacterComponent } from './components/male-character/male-character.component';
-import { FemaleCharacterComponent } from './components/female-character/female-character.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MaleCharacterComponent,
-    FemaleCharacterComponent,
+    AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- frontend/Softoom/src/app/components/female-character/female-character.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-female-character',
-  templateUrl: './female-character.component.html',
-  styleUrls: ['./female-character.component.scss'],
-})
-export class FemaleCharacterComponent {}
