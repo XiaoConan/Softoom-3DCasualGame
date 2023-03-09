30578d6e07bf75eafbe85fa39728518c3088edac
XiaoConan <HsupermarioM@gmail.com>
2023-03-09 01:11:40 -0500

    Created the Female character model

--- frontend/Softoom/src/app/components/female-character/female-character.component.ts
+++ frontend/Softoom/src/app/components/female-character/female-character.component.ts
@@ -1,8 +1,63 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 @Component({
   selector: 'app-female-character',
   templateUrl: './female-character.component.html',
   styleUrls: ['./female-character.component.scss'],
 })
-export class FemaleCharacterComponent {}
+export class FemaleCharacterComponent {
+  constructor() {}
+
+  ngOnInit(): void {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    // Add ambient light to the scene
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.2); // color, intensity
+    scene.add(ambientLight);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio * 2);
+    document.body.appendChild(renderer.domElement);
+
+    //load the female role gltf model
+
+    let model: THREE.Group;
+
+    const loader = new GLTFLoader();
+    loader.load(
+      'assets/female-role/scene.gltf',
+      (gltf) => {
+        model = gltf.scene;
+        gltf.scene.scale.set(0.2, 0.2, 0.2);
+        gltf.scene.position.set(0, -2, 0);
+        scene.add(gltf.scene);
+      },
+      undefined,
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      // Rotate the imported model
+      model.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+    animate();
+  }
+}
