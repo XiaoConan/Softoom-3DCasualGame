92a5b7659f3cd15f0be55f4b8046e50674feca47
XiaoConan <HsupermarioM@gmail.com>
2023-03-17 10:32:49 -0400

    Created the sign in and sign up page

--- frontend/Softoom/src/app/components/sign-up-page/sign-up-page.component.ts
+++ frontend/Softoom/src/app/components/sign-up-page/sign-up-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
-  styleUrls: ['./sign-up-page.component.scss']
+  styleUrls: ['./sign-up-page.component.scss', './sign-up-page-cols.scss']
 })
 export class SignUpPageComponent {
 
