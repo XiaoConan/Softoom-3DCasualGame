38894a0c733b85ce26399e97187cacf4883aade6
XiaoConan <HsupermarioM@gmail.com>
2023-03-16 23:08:08 -0400

    Created the Home page and basic page routing

--- frontend/Softoom/src/app/app-routing.module.ts
+++ frontend/Softoom/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
