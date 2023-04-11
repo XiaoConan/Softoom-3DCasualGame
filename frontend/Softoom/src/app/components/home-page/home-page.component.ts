32894b5ec89f2f4e9c3f26269ce65395bd8c7ac4
XiaoConan <HsupermarioM@gmail.com>
2023-04-11 13:59:57 -0400

    Update home-page.component.ts

--- frontend/Softoom/src/app/components/home-page/home-page.component.ts
+++ frontend/Softoom/src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-home-page',
@@ -8,7 +7,7 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./home-page.component.scss', './home-page-cols.scss'],
 })
 export class HomePageComponent {
-  constructor(private router: Router, public auth: AuthService) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 }
