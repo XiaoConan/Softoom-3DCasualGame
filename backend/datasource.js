685b4d6148ce4483e12e0b680cc436a430df87a6
XiaoConan <HsupermarioM@gmail.com>
2023-04-11 22:05:59 -0400

    update

--- backend/datasource.js
+++ backend/datasource.js
@@ -3,6 +3,6 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize("appData", "postgres", "H1092387456M", {
   host: "db",
-  port: 5433,
+  port: 5432,
   dialect: "postgres",
 });
